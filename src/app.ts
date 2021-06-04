69e071f252e708a10e29d6ef2ffdb8eb6fd2c317
Prasad Medarametla <medarametla.l.v.prasad@gmail.com>
2021-06-04 14:13:23 +0530

    Initial commit - Working blank express template

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port: number = 3000;
+
+app.get('/', (req, res) => {
+    res.send('The sedulous hyena ate the antelope!');
+});
+
+app.listen(port, () => {
+    console.log('Listening on port: ', port);
+}).on('error', (e) => {
+    console.log('Error happened: ', e.message)
+});
